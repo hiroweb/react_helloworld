6e5d97d0c95f2a72420dc7e1862e8648d78ab110
hiroweb <79069531+hiroweb@users.noreply.github.com>
2021-11-20 14:53:21 +0900

    faceButtonAdded

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import ColorfulMessage from "./components/ColofulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>Hello World</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickSwitchShowFlag}>顔ボタン</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^^♪</p>}
     </>
   );
 };
