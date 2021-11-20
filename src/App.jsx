f3155f8de629dca54154a4ec2d23d07e205ff801
hiroweb <79069531+hiroweb@users.noreply.github.com>
2021-11-20 14:47:21 +0900

    AddCountUpButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColofulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
   return (
     <>
-      <h1 style={{ color: "red" }}>hello world</h1>
+      <h1 style={{ color: "red" }}>Hello React</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
