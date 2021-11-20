abb569f15e8c3224c6663bb2cbcb428f8c69c629
hiroweb <79069531+hiroweb@users.noreply.github.com>
2021-11-20 14:40:13 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react_helloworld
+# reactBeginner
 Created with CodeSandbox
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ColorfulMessage from "./components/ColofulMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>hello world</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
