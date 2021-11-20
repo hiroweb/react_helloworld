bf22f8a7740691eca2899eadcda8be32bde3dfee
hiroweb <79069531+hiroweb@users.noreply.github.com>
2021-11-20 15:50:25 +0900

    userEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColofulMessage";
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColofulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   const onClickSwitchShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
   };
+
+  //この変数の変化だけを監視したいときに使う
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
@@ -17,7 +30,8 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
-      <button onClick={onClickSwitchShowFlag}>顔ボタン</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <br />
       <p>{num}</p>
       {faceShowFlag && <p>(^^♪</p>}
     </>
